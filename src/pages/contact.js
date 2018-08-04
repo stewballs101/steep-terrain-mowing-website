8f652963e06e6a681b376bc69bb76e8673a12771
Brad <contact@austere.global>
2018-08-04 16:34:33 +0930

    updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,17 +8,8 @@ const ContactPage = () => (
   <h2>Contact</h2><br></br>
   <h3>Australia</h3><br></br>
   <ul>
-  <li>Phone/WhatsApp: +61 423 001 473</li>
+  <li>Phone: +61 423 001 473</li>
   <li>Email: nathan@austere.global</li>
-  </ul><br></br>
-  <h3>Indonesia</h3><br></br>
-  <ul>
-  <li>Phone/WhatsApp: +61 439 734 323</li>
-  <li>Email: brad@austere.global</li>
-  </ul><br></br>
-  <h3>General</h3><br></br>
-  <ul>
-  <li>Email: contact@austere.global</li>
   </ul><br></br>
   </div>
   
--- src/pages/products.js
+++ src/pages/products.js
@@ -9,17 +9,8 @@ const ProductsPage = () => (
   <div className=" mx-auto sm:w-1/2 p-4">
 
   <div className="text-md">
-   <h2>Services:</h2><br></br>
-   <sup>Click to find out more</sup>
-  <h3><Link to="/services/riskassessment" className="block text-xl font-medium tracking-wide md:inline-block mt-4 md:mt-0 mr-6 no-underline text-grey-darkest">→ Risk Assessments</Link></h3><br></br>
-  <h3><Link to="/services/emergencymgmt" className="block text-xl font-medium tracking-wide md:inline-block mt-4 md:mt-0 mr-6 no-underline text-grey-darkest">→ Emergency Management</Link></h3><br></br>
-  <h3><Link to="/services/physicalconditioning" className="block text-xl font-medium tracking-wide md:inline-block mt-4 md:mt-0 mr-6 no-underline text-grey-darkest">→ Physical Conditioning</Link></h3><br></br>
-  <h3><Link to="/services/wfatraining" className="block text-xl font-medium tracking-wide md:inline-block mt-4 md:mt-0 mr-6 no-underline text-grey-darkest">→ Wilderness First-Aid Training</Link></h3><br></br>
-  <h3><Link to="/services/medical" className="block text-xl font-medium tracking-wide md:inline-block mt-4 md:mt-0 mr-6 no-underline text-grey-darkest">→ Medical Services</Link></h3><br></br>
-  <h3><Link to="/services/security" className="block text-xl font-medium tracking-wide md:inline-block mt-4 md:mt-0 mr-6 no-underline text-grey-darkest">→ Security</Link></h3><br></br>
-  <h3><Link to="/services/mentalhealth" className="block text-xl font-medium tracking-wide md:inline-block mt-4 md:mt-0 mr-6 no-underline text-grey-darkest">→ Mental Health</Link></h3><br></br>
-  <h3><Link to="/services/rescue" className="block text-xl font-medium tracking-wide md:inline-block mt-4 md:mt-0 mr-6 no-underline text-grey-darkest">→ Rescue</Link></h3><br></br>
-  <h3><Link to="/services/evacuation" className="block text-xl font-medium tracking-wide md:inline-block mt-4 md:mt-0 mr-6 no-underline text-grey-darkest">→ Evacuation</Link></h3><br></br>
+   <h2>Products:</h2><br></br>
+
   </div>
   </div>
   <div className="mx-auto md:w-1/2 p-4">
